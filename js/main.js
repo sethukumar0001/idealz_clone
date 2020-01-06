4df1a476f776f687556c76eaee3b5c560c45e169
Fatma-Abdullah <fatmaabdallah78@yahoo.com>
2020-01-06 03:36:01 +0200

    add wishlist of user

--- js/main.js
+++ js/main.js
@@ -322,3 +322,18 @@ $(window).scroll(function() {
         });
     };
 }(jQuery));
+
+// -----------wishlist-------
+// Remove Items From Cart
+$('a.remove').click(function(){
+	event.preventDefault();
+	$( this ).parent().parent().parent().hide( 400 );
+   
+  })
+  
+  // Just for testing, show all items
+	$('a.btn.continue').click(function(){
+	  $('li.items').show(400);
+	})
+  
+	
